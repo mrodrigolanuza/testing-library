55013ab67c02c2131b502b2f9b46fad798754b0a
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2023-01-29 17:59:14 +0100

    Add average function

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
 export function sum(numbers:number[]){
     return numbers.reduce((previous, next)=> previous + next);
 }
+
+export function average(numbers:number[]){
+    return sum(numbers) / numbers.length;
+}
