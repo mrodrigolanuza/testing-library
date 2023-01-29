33432fe9de97d90f88527661820ab5da1c230b18
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2023-01-29 19:12:50 +0100

    Encapsulate functionality in a test function

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,15 +1,18 @@
 import {sum} from "./stats";
 import {average} from "./stats";
-import {expect} from "./testLib"
+import {expect, test} from "./testLib"
 
-let result, expected;
-result = sum([1,2,3]);
-expected = 6;
-expect(expected).toBe(result);
+test("calculate the sum of all elements", ()=>{
+    const result = sum([1,2,3]);
+    const expected = 6;
+    expect(result).toBe(expected);
+});
 
-result = average([1,2,3]);
-expected = 2;
-expect(expected).toBe(result);
+test("calculate the average of all elements", ()=>{
+    const result = average([1,2,3]);
+    const expected = 2;
+    expect(expected).toBe(result);
+});
 
 
 
--- src/testLib.ts
+++ src/testLib.ts
@@ -1,10 +1,20 @@
 export function expect<T>(expected:T){
     return {
         toBe(result:T){
-            if(result === expected)
-                console.log("OK ");
-            else
+            if(result !== expected)
                 throw new Error("NOK ${result} is not equal to ${expected}");
         }
+    }
+}
+
+export function test(description:string, callback:()=>void){
+    try{
+        callback();
+        console.log("OK! " + description);
+    }
+    catch(error)
+    {
+        console.log("NOK " + description);
+        console.log(error);
     }
 }
