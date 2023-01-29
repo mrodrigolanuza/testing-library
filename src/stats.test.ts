5ff25e24703168b83c845bfd815440c821ab08cd
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2023-01-29 18:49:18 +0100

    Add expect assertion

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,19 +1,22 @@
 import {sum} from "./stats";
 import {average} from "./stats";
 
-let result = sum([1,2,3]);
-let expected = 6;
-
-if(result === expected)
-    console.log("OK ");
-else
-    throw new Error("NOK ${result} is not equal to ${expected}");
-
+let result, expected;
+result = sum([1,2,3]);
+expected = 6;
+expect(expected).toBe(result);
 
 result = average([1,2,3]);
 expected = 2;
+expect(expected).toBe(result);
 
-if(result === expected)
-    console.log("OK ");
-else
-    throw new Error("NOK ${result} is not equal to ${expected}");
+function expect(expected){
+    return {
+        toBe(result){
+            if(result === expected)
+                console.log("OK ");
+            else
+                throw new Error("NOK ${result} is not equal to ${expected}");
+        }
+    }
+}
