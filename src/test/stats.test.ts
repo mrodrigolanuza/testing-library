c95467d467bbb5a6fff939ad7f915ef94217ffb8
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2023-01-30 11:31:13 +0100

    Create new file for async tests

--- src/test/stats.test.ts
+++ src/test/stats.test.ts
@@ -1,5 +1,4 @@
 import {sum, average} from "../stats";
-import * as statsAsync from "../statsAsync";
 
 describe("Stats should", ()=>{
     it("calculate the sum of all elements of the array", ()=>{
@@ -14,18 +13,3 @@ describe("Stats should", ()=>{
         expect(expected).toBe(result);
     });
 });
-
-describe("Stats async should", ()=>{
-    it("calculate the sum of all elements of the array (async)", async ()=>{
-        const result = await statsAsync.sum([1,2,3]);
-        const expected = 6;
-        expect(expected).toBe(result);
-    });
-    
-    it("calculate the average of all elements of the array (async)", async ()=>{
-        const result = await statsAsync.average([1,2,3]);
-        const expected = 2;
-        expect(expected).toBe(result);
-    });
-});
-
