5e7699798fc1936b2a3344e4b9c6ca5751511a37
Marcos Rodrigo <marcosrlanuza@hotmail.com>
2023-01-30 18:30:24 +0100

    Configure global or single file coverage threshold percentage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,20 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  verbose: true
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: "./coverage",
+  coverageThreshold: {
+    global:{
+      statements: 50,
+      branches: 50,
+      functions: 50,
+      lines: 50
+    },
+    './src/stats.ts':{
+      statements: 100,
+      branches: 100,
+      functions:100,
+      lines: 100    }
+  } 
 };
